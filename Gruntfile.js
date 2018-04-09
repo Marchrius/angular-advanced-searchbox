9fe6832e8c4620e6b33cad1f786fd6b2b2048ad2
Matteo Gaggiano <maxxxx92@gmail.com>
2018-04-09 10:15:27 +0200

    Added Gruntfile (for local serving purposes) Added package for node deps Updated ignorefile

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          port: 9001,
+          base: 'app'
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+  // Default task.
+  grunt.registerTask('default', ['server']);  
+  grunt.registerTask('server', 'Start a custom web server.', function() {
+    grunt.log.writeln('Starting web server on port 8080.');
+    require('./server.js').listen(8080);
+  });
+
+};
